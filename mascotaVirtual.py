6ee8af8c7584a8b2bb8cedb8702393b9670af0f6
MichelUribe1925 <mic.uribe@duocuc.cl>
2026-05-22 20:46:40 -0400

    mascotaVirtual.py

--- mascotaVirtual.py
+++ mascotaVirtual.py
@@ -1,9 +1,9 @@
-nombre = "waffle"
+nombre = "Waffle"
 energia = 100
 felicidad = 100
-print(f"Energia inicial de: {nombre}. energia: {energia}, felicidad: {felicidad} ")
+print(f"Energía inicial de: {nombre}, energía: {energia}, felicidad: {felicidad}")
 while energia > 0:
-    print("n\¿Que quieres hacer: ?")
+    print("¿Qué quieres hacer:?")
     print("1.- Alimentar")
     print("2.- Jugar")
     print("3.- Ver estado de salud")
@@ -12,17 +12,22 @@ while energia > 0:
     if opcion == "1":
         energia = energia + 20
         felicidad = felicidad + 1
-        print(f"Alimentaste a {nombre}, esta muy feliz contigo.. ")
+        print(f"Alimentaste a {nombre}, está muy feliz contigo..")
     elif opcion == "2":
         energia = energia - 15
         felicidad = felicidad + 20
-        print(f"{nombre} esta cansado")
+        print(f"{nombre} esta cansado pero feliz de jugar contigo")
     elif opcion == "3":
+        print("     /\__                    /\_/\   ")
+        print("    (    @\___              ( o.o )  ")
+        print("    /         O               >^<    ")
+        print("   /     (____/                      ")
+        print("   /_____/                           ")
         print(f"Energia: {energia}")
         print(f"Felicidad: {felicidad}")
     elif opcion == "4":
         felicidad = felicidad - 10
         energia = energia - 5
-        print(f"{nombre} esta muy aburrido")
+        print(f"{nombre} está muy aburrido....")
     else:
-        print("error de ingreso")        
+        print("error de ingreso")
